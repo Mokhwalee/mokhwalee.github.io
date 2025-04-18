c24459cc9c329b7b6245315a5cad760078ee158e
Mokhwalee <54646781+Mokhwalee@users.noreply.github.com>
2025-04-18 19:05:31 +0000

    Deploying to gh-pages from @ Mokhwalee/mokhwalee.github.io@2900d6b6fd2eb7090f2b21dbb5f136509152487a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "To learn more about my background and experience, click the purple PDF button on the right :)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
