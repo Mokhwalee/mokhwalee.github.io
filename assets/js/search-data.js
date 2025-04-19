560ff98d407e843d71692bfe3318b05f2ae55192
Mokhwalee <54646781+Mokhwalee@users.noreply.github.com>
2025-04-19 21:49:25 +0000

    Deploying to gh-pages from @ Mokhwalee/mokhwalee.github.io@e3f89deffa9e01eacace096ea66ae605bcb3fcda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-repositories",
+          title: "repositories",
           description: "",
           section: "Navigation",
           handler: () => {
