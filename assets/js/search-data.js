aceb891328e9a4d521006bbd0a9f7b976764eade
Mokhwalee <54646781+Mokhwalee@users.noreply.github.com>
2025-04-20 03:20:23 +0000

    Deploying to gh-pages from @ Mokhwalee/mokhwalee.github.io@a134be110a6e8939e36bfa5a1c784632dc730d02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,18 +416,13 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-github",
-          title: 'Github',
+          section: "News",},{id: "projects-poster",
+          title: 'Poster',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-ieee-ppt",
           title: 'IEEE PPT',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/22_project/";
-            },},{id: "projects-poster",
-          title: 'Poster',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
